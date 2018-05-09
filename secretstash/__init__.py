d6e70c7a5ba8883b17ff87ad29f9d618369de4c6
Christopher Armstrong <chris.armstrong@juiceanalytics.com>
2018-05-09 16:20:40 -0500

    I typed some bytes into a file

---
+++ secretstash/__init__.py
@@ -0,0 +1,37 @@
+"""
+See get_secret
+"""
+
+import toml
+from credstash import getSecret
+
+__all__ = ['get_secret']
+
+
+def dotted_get(obj, name):
+    parts = name.split('.')
+    cur = obj
+    for part in parts:
+        cur = cur[part]
+    return cur
+
+
+class SecretStash(object):
+    def __init__(self, region, local_filename):
+        self.region = region
+        self.local_filename = local_filename
+
+    def get_local(self, name):
+        try:
+            with open(self.local_filename) as f:
+                data = toml.loads(f.read())
+                dotted_get(data, name)
+        except IOError
+            pass
+
+    def get_secret(self, name, context):
+        secret = self.get_local(name)
+        if secret is None:
+            return getSecret(name, region=self.region, context=context)
+        else:
+            return secret
